8f8e13ada08222d52dfe26d0b88515021cafe000
Luka Rakocevic <dr.dexxter@gmail.com>
2022-07-02 20:07:08 +0200

    Backend added and revised, cityName respo

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 const express = require ("express");
-const res = require("express/lib/response");
 const https = require ("https");
+const bodyParser = require ("body-parser");
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=belgrade&units=metric&appid=b6a72cdd653e69ebe74dfcb51937a534";
-
-    https.get(url, function(response){
-        console.log(response.statusCode);
-
-        response.on("data", function(data){
-           const weatherData = JSON.parse(data)
-           const temp = weatherData.main.temp
-           console.log(temp);
-            const weatherDescription = weatherData.weather[0].description
-            const weatherID = weatherData.weather[0].icon;
-            console.log(weatherID);
-
-            res.set("Content-Type", "text/html");
-            res.write("<img src='"+"http://openweathermap.org/img/wn/" + weatherID + "@2x.png" + "'>");
-            res.write("<p>The weather is "+ weatherDescription + " over the city</p>");
-            res.write("<h1>The temperature in Beograd is "+temp + " dungarees Celsius</h1>")
-            res.send();
-        })
+    res.sendFile(__dirname +"/index.html")
     });
+
+app.post("/", function (req, res){
+
+    const query = req.body.cityName
+    const apiKey = "b6a72cdd653e69ebe74dfcb51937a534"
+    const units = "metric"
+ const url = "https://api.openweathermap.org/data/2.5/weather?q="
+ +query+"&appid="+apiKey+"&units="+units;
+
+
+ https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+     const weatherData = JSON.parse(data)
+    const temp = weatherData.main.temp
+    const desc = weatherData.weather[0].description
+    const icon = weatherData.weather[0].icon
+    const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
+        
+  res.write("<h1>The conditions are "+desc+"</h1>");
+  res.write("<h1>The temperature in "+query+" is "+ temp +" dungarees celsius. </h1>");
+  res.write("<img src="+imageURL+">");
+  res.send()
+
+
+    })
+    })
 
 
 })
-
-
 app.listen(3000, function(){
-    console.log("server is runnming on port 3000.")
+console.log("Server is runnming on port 3000.")
 });
-
